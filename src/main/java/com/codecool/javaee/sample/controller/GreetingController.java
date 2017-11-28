6fdb7f9951edd7152e4513fa920f473db8d75b26
zoltansallay <zoltan.sallay@codecool.com>
2017-11-28 19:49:46 +0100

    Some rename.

--- src/main/java/com/codecool/javaee/sample/controller/GreetingController.java
+++ src/main/java/com/codecool/javaee/sample/controller/GreetingController.java
@@ -1,7 +1,5 @@
 package com.codecool.javaee.sample.controller;
 
-import com.codecool.javaee.sample.model.Customer;
-
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -9,14 +7,14 @@ import java.io.Serializable;
 @Named
 @SessionScoped
 public class GreetingController implements Serializable {
-    private String actualName = "World";
+    private String name = "World";
 
     public String getName() {
-        return actualName;
+        return name;
     }
 
     public void setName(String name) {
-        actualName = name;
+        this.name = name;
     }
 
 }
